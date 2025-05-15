98b1df4c61084e0bdb1dd69dd27206da4dcf94ad
Peter Abahim <lilpero03@gmail.com>
2025-05-15 01:42:38 +0100

    just finish adding comment input

--- dist/script.js
+++ dist/script.js
@@ -9,4 +9,20 @@ function toggleMenu() {
     menu.classList.add("hidden");
     icon.setAttribute("name", "menu-outline");
   }
-}
+};
+
+
+document.querySelectorAll('#button').forEach((button, index) => {
+  button.addEventListener('click', function () {
+    const messageInput = document.querySelectorAll('#message')[index]; 
+    const message = messageInput.value;
+    const submitDiv = document.querySelectorAll('#submit')[index]; 
+    if (message.trim() !== '') {
+      const p = document.createElement('p');
+      p.className = 'mt-2 text-start break-words';
+      p.textContent = message;
+      submitDiv.appendChild(p);
+      messageInput.value = ''; 
+    }
+  });
+});
